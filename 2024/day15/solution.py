e875ae75165e66e3c75d1cbd942b52d19291059e
Leo Kim <leoybkim@gmail.com>
2024-12-16 23:33:32 -0500

    solution for day 15 part 2 WIP

--- 2024/day15/solution.py
+++ 2024/day15/solution.py
@@ -1,3 +1,4 @@
+import copy
 from typing import List
 
 
@@ -7,6 +8,13 @@ def read_file(input_file_path: str) -> str:
 
 
 def format_data(raw_data: str, large: bool) -> (List[List[str]], List[str]):
+    """
+    Read input and returns the map and robot movement directions parsed from the file.
+    If large box mode, widen the map by replacing the legends with corresponding characters of double length.
+    @param raw_data: Input file
+    @param large: Large box mode
+    @return: Map and robot directions
+    """
     grid_str, moves_str = raw_data.split("\n\n")
     grid = []
     moves = []
@@ -15,13 +23,13 @@ def format_data(raw_data: str, large: bool) -> (List[List[str]], List[str]):
             temp = []
             for c in list(line):
                 if c == "#":
-                    temp.append("##")
+                    temp += ["#", "#"]
                 elif c == "O":
-                    temp.append("[]")
+                    temp += ["[", "]"]
                 elif c == ".":
-                    temp.append("..")
+                    temp += [".", "."]
                 elif c == "@":
-                    temp.append("@.")
+                    temp += ["@", "."]
             grid.append(temp)
         else:
             grid.append(list(line))
@@ -32,61 +40,139 @@ def format_data(raw_data: str, large: bool) -> (List[List[str]], List[str]):
 
 
 def robot_coordinate(grid: List[List[str]]) -> tuple[int, int]:
+    """
+    Returns the current coordinate of the robot indicated with "@"
+    @param grid: Map
+    @return: (row, column) coordinate of the Robot
+    """
     for r in range(len(grid)):
         for c in range(len(grid[0])):
             if grid[r][c] == "@":
                 return r, c
 
 
-def push_boxes(grid: List[List[str]], robot: tuple[int, int], d: tuple[int, int]) -> bool:
+def recursive_push(grid: List[List[str]], row: int, boxes: tuple[int, int], dr: int, prev: tuple[int, int] | None,
+                   prev_row: List[str] | None, edge: str) -> bool:
+    """
+    Returns True if the boxes can be pushed at the current level
+    @param grid: Temporary working copy of the map
+    @param row: Current row
+    @param boxes: Start and end indices of the boxes
+    @param dr: Vertical direction of the robots movement
+    @param prev: Start and end indices of the previous boxes
+    @param prev_row: Copy of the previous row
+    @param edge: "[" or "]" edge of the box being pushed
+    @return: True if the boxes can be pushed, False otherwise
+    """
+    s, e = boxes
+    temp_prev_row = grid[row].copy()
+    if any(grid[row + dr][i] == "#" for i in range(s, e + 1)):
+        # Wall is blocking some of the boxes, cannot push
+        return False
+
+    # Find next row of boxes, if any
+    ns = ne = None
+    if grid[row + dr][s] == "]":
+        ns = s - 1
+    else:
+        for i in range(s, e + 1):
+            if grid[row + dr][i] == "[":
+                ns = i
+                break
+
+    if grid[row + dr][e] == "[":
+        ne = e + 1
+    else:
+        for i in range(e, s - 1, -1):
+            if grid[row + dr][i] == "]":
+                ne = i
+                break
+
+    if prev is None:
+        grid[row][s:e + 1] = ["@", "."] if edge == "[" else [".", "@"]
+        if edge == "[":
+            grid[row - dr][s] = "."
+        else:
+            grid[row - dr][e] = "."
+    else:
+        ps, pe = prev
+        if ps <= s and pe <= e:
+            grid[row][ps:pe + 1] = prev_row[ps:pe + 1]
+        else:
+            grid[row][s:e + 1] = ["."] * (abs(ps - s) - 1) + prev_row[s:e + 1] + ["."] * (abs(e - pe) - 1)
+    if ns is None and ne is None:
+        grid[row + dr][s:e + 1] = temp_prev_row[s:e + 1]
+        return True
+    else:
+        return recursive_push(grid, row + dr, (ns, ne), dr, (s, e), temp_prev_row, edge)
+
+
+def push_boxes(grid: List[List[str]], robot: tuple[int, int], d: tuple[int, int], large: bool) -> bool:
     """
     Push the boxes by 1 cell in direction of the robots movement originating from the current position.
+    When pushing larger boxes, push all boxes in the chain that are consecutively affected.
+    Prevent the push if any box in the chain is blocked by a wall.
     @param grid: Map
     @param robot: Current coordinate of the robot
     @param d: Direction of the robots movement (r, c)
+    @param large: Larger box mode
     @return: True if boxes were pushed, False otherwise
     """
-    if d[0] == -1:
-        # Check if enough space vertically above the boxes before reaching the wall
-        for r in range(robot[0] - 1, 0, -1):
-            if grid[r][robot[1]] == "#":
+    rr, rc = robot
+    dr, dc = d
+    edge = grid[rr + dr][rc]  # "0" or "[" or "]"
+    s = rc - (edge == "]")
+    e = rc + (edge == "[")
+    if dr != 0:
+        # Check if enough space vertically above or below the boxes before reaching the wall
+        for r in range(rr + dr, (len(grid) - 1) if dr == 1 else 0, dr):
+            if grid[r][rc] == "#":
                 return False
-            if grid[r][robot[1]] == ".":
-                # Swap the position of the first box and the first available space
-                grid[r][robot[1]], grid[robot[0] - 1][robot[1]] = grid[robot[0] - 1][robot[1]], grid[r][robot[1]]
-                return True
-    elif d[0] == 1:
-        # Check if enough space vertically below the boxes before reaching the wall
-        for r in range(robot[0] + 1, len(grid) - 1, 1):
-            if grid[r][robot[1]] == "#":
-                return False
-            if grid[r][robot[1]] == ".":
-                # Swap the position of the first box and the first available space
-                grid[r][robot[1]], grid[robot[0] + 1][robot[1]] = grid[robot[0] + 1][robot[1]], grid[r][robot[1]]
-                return True
-    elif d[1] == -1:
-        # Check if enough space horizontally left of the boxes before reaching the wall
-        for c in range(robot[1] - 1, 0, -1):
-            if grid[robot[0]][c] == "#":
-                return False
-            if grid[robot[0]][c] == ".":
-                # Swap the position of the first box and the first available space
-                grid[robot[0]][c], grid[robot[0]][robot[1] - 1] = grid[robot[0]][robot[1] - 1], grid[robot[0]][c]
-                return True
-    elif d[1] == 1:
-        # Check if enough space horizontally right of the boxes before reaching the wall
-        for c in range(robot[1] + 1, len(grid[0]) - 1, 1):
-            if grid[robot[0]][c] == "#":
-                return False
-            if grid[robot[0]][c] == ".":
-                # Swap the position of the first box and the first available space
-                grid[robot[0]][c], grid[robot[0]][robot[1] + 1] = grid[robot[0]][robot[1] + 1], grid[robot[0]][c]
-                return True
+            if grid[r][rc] == ".":
+                # Shift all the boxes in the column one cell vertically in the direction
+                if large:
+                    # Don't really like this code design... How do I revert if I want to edit the original map?
+                    # Create a copy of the current map and attempt to push boxes recursively for each row
+                    # If all rows of boxes can be pushed, reassigned the edited map to the current map
+                    # Otherwise, toss the temp map
+                    temp = copy.deepcopy(grid)
+                    if recursive_push(temp, rr + dr, (s, e), dr, None, None, edge):
+                        grid.clear()
+                        grid.extend(temp)
+                        return True
+                    else:
+                        return False
+                else:
+                    if dr == -1:
+                        for i in range(r, rr - 1):
+                            grid[i][rc] = grid[i + 1][rc]
+                    elif dr == 1:
+                        for i in range(r, rr + 1, -1):
+                            grid[i][rc] = grid[i - 1][rc]
 
+                return True
+    elif dc != 0:
+        # Check if enough space horizontally left or right of the boxes before reaching the wall
+        for c in range(rc + dc, (len(grid[0]) - 1) if dc == 1 else 0, dc):
+            if grid[rr][c] == "#":
+                return False
+            if grid[rr][c] == ".":
+                # Shift all the boxes in the row one cell horizontally in the direction
+                if dc == -1:
+                    grid[rr][c:rc] = grid[rr][c + 1:rc] + ["."]
+                elif dc == 1:
+                    grid[rr][rc + 1:c + 1] = ["."] + grid[rr][rc + 1:c]
+                return True
     return False
 
 
-def move_robot(grid: List[List[str]], moves: List[str]):
+def move_robot(grid: List[List[str]], moves: List[str], large: bool):
+    """
+    Moves the robot as indicated in the direction while pushing boxes
+    @param grid: Map
+    @param moves: Direction of robot movements
+    @param large: Large box mode
+    """
     coord = {
         "<": (0, -1),
         "^": (-1, 0),
@@ -94,29 +180,47 @@ def move_robot(grid: List[List[str]], moves: List[str]):
         "v": (1, 0)
     }
     rr, rc = robot_coordinate(grid)
-    for move in moves:
+    for i, move in enumerate(moves):
         nr, nc = rr + coord[move][0], rc + coord[move][1]
         if 0 < nr < len(grid) - 1 and 0 < nc < len(grid[0]) - 1:  # Borders are surrounded by walls of 1 cell depth
             if grid[nr][nc] == "#":
                 continue  # Wall, do nothing
-            elif grid[nr][nc] == "O":
+            elif grid[nr][nc] in ["O", "[", "]"]:
+                box = grid[nr][nc]
                 # Box, check if boxes can be pushed and move
-                if push_boxes(grid, (rr, rc), coord[move]):
+                if push_boxes(grid, (rr, rc), coord[move], large):
+                    if move == "^" or move == "v":
+                        if box == "[":
+                            grid[nr][nc + 1] = "."
+                        if box == "]":
+                            grid[nr][nc - 1] = "."
                     grid[rr][rc], grid[nr][nc] = ".", "@"
                     rr, rc = nr, nc
             else:
                 grid[rr][rc], grid[nr][nc] = ".", "@"
                 rr, rc = nr, nc
+        print(f"{i}th Move: {move}")
+        for line in grid:
+            print(line)
+        print("======================================")
 
 
 def sum_gps_coordinates(raw_input: str, large=False) -> int:
+    """
+    Returns the sum of all boxes' GPS coordinates.
+    GPS coordinate of a box is equal to 100 x y coordinate + x coordinate where top left corner of the map is (0,0).
+    @param raw_input: Input file
+    @param large: Large box mode
+    @return: Sum of all boxes' GPS coordinates
+    """
     grid, moves = format_data(raw_input, large)
-    move_robot(grid, moves)
+    move_robot(grid, moves, large)
     s = 0
     for r in range(len(grid)):
         for c in range(len(grid[0])):
-            if grid[r][c] == "O":
+            if grid[r][c] == ("[" if large else "O"):
                 s += (100 * r + c)
+
     return s
 
 
--- 2024/day15/test.py
+++ 2024/day15/test.py
@@ -6,8 +6,9 @@ from solution import *
 class Test(unittest.TestCase):
     def setUp(self):
         self.input_file = read_file("inputs/input.txt")
-        self.test1_file = read_file("inputs/test1.txt")
-        self.test2_file = read_file("inputs/test2.txt")
+        self.test_file1 = read_file("inputs/test1.txt")
+        self.test_file2 = read_file("inputs/test2.txt")
+        self.test_file3 = read_file("inputs/test3.txt")
 
     def testPart1(self):
         self.assertEqual(1415498, sum_gps_coordinates(self.input_file))
@@ -16,10 +17,17 @@ class Test(unittest.TestCase):
         pass
 
     def testPart1Example1(self):
-        self.assertEqual(10092, sum_gps_coordinates(self.test1_file))
+        self.assertEqual(10092, sum_gps_coordinates(self.test_file1))
 
     def testPart1Example2(self):
-        self.assertEqual(2028, sum_gps_coordinates(self.test2_file))
+        self.assertEqual(2028, sum_gps_coordinates(self.test_file2))
+
+    def testPart2Example1(self):
+        self.assertEqual(618, sum_gps_coordinates(self.test_file3, large=True))
+
+    def testPart2Example2(self):
+        self.assertEqual(9021, sum_gps_coordinates(self.test_file1, large=True))
+
 
 if __name__ == "__main__":
     unittest.main()
