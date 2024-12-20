2ef107304974a3ce5ce94af281e39bac52d473ce
Leo Kim <leoybkim@gmail.com>
2024-12-19 19:08:26 -0500

    solution for day 16 part2

--- 2024/day16/solution.py
+++ 2024/day16/solution.py
@@ -14,33 +14,37 @@ def format_data(raw_data: str) -> List[List[str]]:
     return maze
 
 
-def dijkstra(maze: List[List[str]], init: tuple[int, int], goal: tuple[int, int]) -> int:
+def dijkstra(maze: List[List[str]], init: tuple[int, int], goal: tuple[int, int]) -> tuple:
     """
-    Return the lowest score possible for solving the maze.
+    Return the lowest score possible and all best paths
     Each step forward adds 1 score and a turn adds additional 1000 score.
     @param maze: Map of the maze
     @param init: Coordinate of the starting cell
     @param goal: Coordinate of the destination cell
-    @return: Return the lowest score if the maze can be solved, otherwise return -1
+    @return: Return the best score and best paths
     """
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # down, up, left, right
     visited = [[[False for _ in range(4)] for _ in range(len(maze[0]))] for _ in range(len(maze))]  # dir, r, c
-    distances = [[[float("inf") for _ in range(4)] for _ in range(len(maze[0]))] for _ in range(len(maze))]  # dir, r, c
+    costs = [[[float("inf") for _ in range(4)] for _ in range(len(maze[0]))] for _ in range(len(maze))]  # dir, r, c
     r, c = init
-    pq = [(0, r, c, directions[3])]  # (cost, row, column, direction) Start facing east
+    pq = [(0, r, c, directions[3], [init])]  # (cost, row, column, direction, path) Start facing east
 
-    distances[r][c][3] = 0
+    costs[r][c][3] = 0
+    best_paths = []
+    best_cost = float("inf")
 
     def in_bound(row: int, column: int) -> bool:
         return 0 <= row < len(maze) and 0 <= column < len(maze[0])
 
     while pq:
         # Pop and return smallest item from heap, maintaining the heap invariant
-        cur_cost, cur_r, cur_c, cur_dir = heapq.heappop(pq)
+        cur_cost, cur_r, cur_c, cur_dir, path = heapq.heappop(pq)
 
-        # Maze is solved, return the cost
+        # Maze is solved, keep track of best paths
         if (cur_r, cur_c) == goal:
-            return cur_cost
+            if cur_cost <= best_cost:
+                best_cost = cur_cost
+                best_paths.append(path)
 
         visited[cur_r][cur_c][directions.index(cur_dir)] = True
 
@@ -48,13 +52,15 @@ def dijkstra(maze: List[List[str]], init: tuple[int, int], goal: tuple[int, int]
             nr, nc = cur_r + dr, cur_c + dc
             if in_bound(nr, nc) and not visited[nr][nc][i] and maze[nr][nc] == "." or maze[nr][nc] == "E":
                 new_cost = cur_cost + (1 if cur_dir == (dr, dc) else 1001)
-                if new_cost < distances[nr][nc][i]:
-                    distances[nr][nc][i] = new_cost
-                    heapq.heappush(pq, (new_cost, nr, nc, (dr, dc)))
-    return -1
+                if new_cost < costs[nr][nc][i]:
+                    costs[nr][nc][i] = new_cost
+                    heapq.heappush(pq, (new_cost, nr, nc, (dr, dc), path + [(nr, nc)]))
+                elif new_cost == costs[nr][nc][i]:
+                    heapq.heappush(pq, (new_cost, nr, nc, (dr, dc), path + [(nr, nc)]))
+    return best_cost, best_paths
 
 
-def solve_maze(raw_file: str) -> int:
+def solve_maze(raw_file: str, multiple=False) -> int:
     maze = format_data(raw_file)
     start = end = None
     for r in range(len(maze)):
@@ -64,9 +70,18 @@ def solve_maze(raw_file: str) -> int:
             if maze[r][c] == "E":
                 end = (r, c)
 
-    return dijkstra(maze, start, end)
+    best_cost, best_paths = dijkstra(maze, start, end)
+    if multiple:
+        unique = set()
+        for path in best_paths:
+            for cell in path:
+                unique.add(cell)
+        return len(unique)
+    else:
+        return best_cost
 
 
 if __name__ == "__main__":
     file = read_file("inputs/input.txt")
     print(f"Lowest score possible: {solve_maze(file)}")
+    print(f"Total number of tiles on the optimal path: {solve_maze(file, multiple=True)}")
--- 2024/day16/test.py
+++ 2024/day16/test.py
@@ -8,12 +8,13 @@ class Test(unittest.TestCase):
         self.input_file = read_file("inputs/input.txt")
         self.test_file1 = read_file("inputs/test1.txt")
         self.test_file2 = read_file("inputs/test2.txt")
+        self.test_file3 = read_file("inputs/test3.txt")
 
     def testPart1(self):
         self.assertEqual(111480, solve_maze(self.input_file))
 
     def testPart2(self):
-        pass
+        self.assertEqual(529, solve_maze(self.input_file, multiple=True))
 
     def testPart1Example1(self):
         self.assertEqual(7036, solve_maze(self.test_file1))
@@ -22,10 +23,13 @@ class Test(unittest.TestCase):
         self.assertEqual(11048, solve_maze(self.test_file2))
 
     def testPart2Example1(self):
-        pass
+        self.assertEqual(45, solve_maze(self.test_file1, multiple=True))
 
     def testPart2Example2(self):
-        pass
+        self.assertEqual(64, solve_maze(self.test_file2, multiple=True))
+
+    def testPart2Example3(self):
+        self.assertEqual(5, solve_maze(self.test_file3, multiple=True))
 
 
 if __name__ == "__main__":
