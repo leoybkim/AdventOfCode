ea8b0213d48f6686e0e4c6a89174c15e72340597
Leo Kim <leoybkim@gmail.com>
2025-01-01 23:40:49 -0500

    solution for day 22

--- 2024/day22/test.py
+++ 2024/day22/test.py
@@ -6,19 +6,20 @@ from solution import *
 class Test(unittest.TestCase):
     def setUp(self):
         self.input_file = read_file("inputs/input.txt")
-        self.test_file = read_file("inputs/test.txt")
+        self.test_file1 = read_file("inputs/test1.txt")
+        self.test_file2 = read_file("inputs/test2.txt")
 
     def testPart1(self):
-        pass
+        self.assertEqual(13004408787, sum_secrets(self.input_file, iteration=2000))
 
     def testPart2(self):
-        pass
+        self.assertEqual(1455, most_bananas(self.input_file, iteration=2000))
 
     def testPart1Example1(self):
-        self.assertEqual(37327623, sum_secrets(self.test_file, iteration=2000))
+        self.assertEqual(37327623, sum_secrets(self.test_file1, iteration=2000))
 
     def testPart2Example1(self):
-        pass
+        self.assertEqual(23, most_bananas(self.test_file2, iteration=2000))
 
 
 if __name__ == "__main__":
